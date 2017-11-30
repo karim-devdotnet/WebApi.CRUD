0e78b8c821516df18b5d7ac74cbc92c2c1c1efee
karimabd84 <abdelkarim.esti@gmail.com>
2017-11-30 11:44:52 +0100

    added

--- WebApi.CRUD/Controllers/ValuesController.cs
+++ WebApi.CRUD/Controllers/ValuesController.cs
@@ -1,39 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApi.CRUD.Models;
 
 namespace WebApi.CRUD.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        private ValueContext context = null;
+
+        public ValuesController()
         {
-            return new string[] { "value1", "value2" };
+            context = new ValueContext(); //ConfigurationManager.ConnectionStrings["valueContext"].ConnectionString
+        }
+
+        // GET api/values
+        public IEnumerable<Value> Get()
+        {
+            return context.ValueSet;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Value Get(Guid id)
         {
-            return "value";
+            return context.ValueSet.Find(id);
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post(string value)
         {
+            context.ValueSet.Add(new Value { Text = value });
+            context.SaveChanges();
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(Guid id, string value)
         {
+            Value entity = context.ValueSet.Find(id);
+            if (entity != null)
+                entity.Text = value;
+            else
+                context.ValueSet.Add(new Value { Text = value });
+
+            context.SaveChanges();
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public void Delete(Guid id)
         {
+            Value entity = context.ValueSet.Find(id);
+            if(entity != null)
+            {
+                context.ValueSet.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && context != null)
+            {
+                context.Dispose();
+                context = null;
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
---
+++ WebApi.CRUD/Models/ValueContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.CRUD.Models
+{
+    public class ValueContext : DbContext
+    {
+        public ValueContext() : base(nameOrConnectionString: "valueContext")
+        {
+            Database.SetInitializer<ValueContext>(new ValueDbInitializer());
+        }
+
+
+        public ValueContext(string connectionString):base(connectionString)
+        {
+            Database.SetInitializer<ValueContext>(new ValueDbInitializer());
+        }
+
+        public DbSet<Value> ValueSet { get; set; }
+    }
+
+    internal class ValueDbInitializer : CreateDatabaseIfNotExists<ValueContext>
+    {
+        protected override void Seed(ValueContext context)
+        {
+            try
+            {
+                List<string> values = new List<string> { "Value11", "Value12", "Value13", "Value14", "Value15", "Value16", "Value17", "Value18", "Value19" };
+
+                foreach (var item in values)
+                {
+                    context.ValueSet.Add(new Value { Text = item });
+                }
+
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            base.Seed(context);
+        }
+    }
+
+    public class Value
+    {
+        public Value()
+        {
+            Id = Guid.NewGuid();
+            Timestamp = DateTime.Now;
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+        [MaxLength(100)]
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
